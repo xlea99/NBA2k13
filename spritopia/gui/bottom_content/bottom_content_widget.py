71cd32c795f642231a31111102dfc023ed0ac79e
Alex Somheil <alexlsomheil@gmail.com>
2024-06-07 14:41:36 -0500

    Refactored how outer content is structured, added barbones menu navigation support.

--- spritopia/gui/bottom_content/bottom_content_widget.py
+++ spritopia/gui/bottom_content/bottom_content_widget.py
@@ -9,22 +9,3 @@ class BottomContentWidget(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setObjectName("BottomContentWidget")
-        self.setContentsMargins(0,0,0,0)
-
-
-        # Central widget setup
-        self.centralWidget = QWidget(self)
-        self.setCentralWidget(self.centralWidget)
-        self.layout = QVBoxLayout(self.centralWidget)  # Main layout
-        self.layout.setSpacing(0)
-        self.layout.setContentsMargins(0, 0, 0, 0)
-
-        self.setStyleSheet("""
-        #BottomContentWidget {
-            background-color: #1e3059;
-        }
-        """)
-
-        #self.premierGameOptions = PremierGameOptions() #TODO TEMP DISABLED
-        #self.layout.addWidget(self.premierGameOptions)
