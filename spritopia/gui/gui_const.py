e745d22e3bd728ef6265bfac5b96253e07135593
Alex Somheil <alexlsomheil@gmail.com>
2024-07-22 01:35:47 -0500

    added some more standardization support for colors

---
+++ spritopia/gui/gui_const.py
@@ -0,0 +1,14 @@
+ARCHETYPE_COLORS = {"Slayer" : "blue",
+                    "Vigilante" : "green",
+                    "Medic" : "red",
+                    "Guardian" : "yellow",
+                    "Engineer" : "orange",
+                    "Director" : "purple",
+                    "None" : "white"}
+
+RARITY_COLORS = {"Common": "white",
+                 "Rare": "blue",
+                 "Epic": "purple",
+                 "Legendary": "orange",
+                 "Godlike": "red",
+                 "Legacy": "green"}
